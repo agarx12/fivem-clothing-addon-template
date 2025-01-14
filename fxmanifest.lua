d9174c0f7590e2aca508f4ec2beb31805e5afa55
agarx12 <alagreiby1@gmail.com>
2025-01-14 14:35:27 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,8 @@
-fx_version 'cerulean'
-game 'gta5'
+fx_version 'january2016'
+game 'fivem'
 
 files {
-    'mp_f_freemode_01_your_clothing_pack.meta'
+    'mp_m_freemode_01_mp_m_january2016^hand'
 }
 
 data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_your_clothing_pack.meta'
